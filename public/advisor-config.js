2af927a84c708b11f84a80de8eb5b74a31e562bd
Peter-Diamond-LTD <peter@ustaxs.com>
2026-09-21 11:03:02 -0700

    Add Peter branding and advisor profile

---
+++ public/advisor-config.js
@@ -0,0 +1,10 @@
+// Copy this project and change this profile for another CBE.
+window.ADVISOR_PROFILE = Object.freeze({
+  id: 'peter',
+  directAnalysis: true,
+  projectName: 'Peter Calculator',
+  displayName: 'Peter Diamond, CBE®',
+  logo: '/peter-banner.png',
+  logoAlt: 'Peter Diamond supplied branding',
+  hiddenPartnerActions: ['dscrLender', 'sellDeal']
+});
